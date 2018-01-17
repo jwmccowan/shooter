8ced9fd45a30bcd135e621e22c80cb2cee2b6e5c
John McCowan <jwmccowan@gmail.com>
2018-01-18 00:31:41 +1300

    removed some uneccessary files and add a steerable boat.  need to improve the physics.  fixes #2

--- src/index.js
+++ src/index.js
@@ -4,29 +4,19 @@ var map;
 var controls;
 var cursors;
 
+var playerboat;
+
 function preload() {
     this.load.image('tiles', 'assets/tiles_sheet.png');
     this.load.tilemapJSON('map', 'assets/piratemap.json');
+    this.load.atlas('playerboat', 'assets/shipsMiscellaneous_sheet.png', 'assets/spritesheet.json');
 }
 
 function create() {
-    // Start up the physics using ARCADE physics
-    // TODO: this doesn't work in v3
-    // game.physics.startSystem(Phaser.Physics.ARCADE);
+    map = createMap();
+    playerboat = createPlayerBoat();
 
-    map = this.make.tilemap({ key: 'map' });
-    var tiles = map.addTilesetImage('piratemaptileset','tiles');
-
-    var waterLayer = map.createStaticLayer(0, tiles, 0, 0);
-    var shoreLayer = map.createStaticLayer(1, tiles, 0, 0);
-    var foliageLayer = map.createStaticLayer(2, tiles, 0, 0);
-
-
-
-
-    // this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
-    // var cursors = this.input.keyboard.createCursorKeys();
+    cursors = this.input.keyboard.createCursorKeys();
     // var controlConfig = {
     //     camera: this.cameras.main,
     //     left: cursors.left,
@@ -49,8 +39,41 @@ function create() {
 
 }
 
-function update(time,delta) {
+function createMap() {
+    let m = this.make.tilemap({ key: 'map' });
+    var tiles = m.addTilesetImage('piratemaptileset','tiles');
 
+    var waterLayer = m.createStaticLayer(0, tiles, 0, 0);
+    var shoreLayer = m.createStaticLayer(1, tiles, 0, 0);
+    var foliageLayer = m.createStaticLayer(2, tiles, 0, 0);
+    return m;
+}
+
+function createPlayerBoat() {
+    let p = this.add.sprite(200,300,'playerboat',"ship (2).png");
+    this.physics.world.enable([p]);
+    return p;
+}
+
+function update(time,delta) {
+    updatePlayerBoat();
+}
+
+function updatePlayerBoat() {
+    playerboat.body.angularVelocity = 0;
+    playerboat.body.velocity.x = 0;
+    playerboat.body.velocity.y = 0;
+
+    if (cursors.left.isDown) {
+        playerboat.body.angularVelocity -= 100;
+    } else if (cursors.right.isDown) {
+        playerboat.body.angularVelocity += 100;
+    }
+
+    if (cursors.up.isDown) {
+        console.log("eggs",this.physics)
+        this.physics.world.velocityFromAngle(playerboat.angle + 90, 100, playerboat.body.velocity);
+    }
 }
 
 var config = {
@@ -59,6 +82,13 @@ var config = {
     height: 640,
     backgroundColor: '#1b262c',
     pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: { y: 0 }
+        }
+    },
     scene: {
         preload: preload,
         create: create,
